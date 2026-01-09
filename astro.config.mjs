896fbb99dabcc2a3e0b9b1eb955fd0820582ebdf
Raydberg Gabriel <raydbergg@gmail.com>
2026-01-09 11:20:11 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import solidJs from '@astrojs/solid-js';
+
+import vercel from '@astrojs/vercel';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://cosmeticos-catalogo.vercel.app',
+  integrations: [solidJs(), sitemap()],
+  adapter: vercel(),
+  image: {
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+    ],
+  },
+});
